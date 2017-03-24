8cf08bf8e97b71de04d881ee0342738dc9361ad7
Michael Noronha <michaeltnoronha@gmail.com>
2017-03-23 20:11:50 -0500

    Remove accidentially committed executable, updated gitignore

--- snake.c
+++ snake.c
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include <string.h>
 
 typedef WINDOW Window;
 
@@ -7,10 +8,12 @@ int main(){
     initscr();
     cbreak();
     noecho();
+    curs_set(0);
 
-    int yMax, xMax, borderTB, borderLR, menuHeight=80, menuWidth=40;
+    int yMax, xMax, borderTB, borderLR, menuHeight=10, menuWidth=40;
     float relSize = 1.5; // ~1/3 of terminal should be border
     getmaxyx(stdscr,yMax,xMax);
+    printw("%d %d",yMax,xMax);
     if(yMax <= menuHeight || xMax <= menuWidth){
         printw("Terminal window too small!");
         getch();
@@ -21,17 +24,18 @@ int main(){
     Window *menuwin = newwin(menuHeight,menuWidth,(yMax-menuHeight)/2,(xMax-menuWidth)/2);
     refresh();
     box(menuwin,0,0);
+    char* title = "Snake";
+    attron(A_BOLD);
+    mvwprintw(menuwin,1,(menuWidth-strlen(title))/2,title);
+    attroff(A_BOLD);
+
+
+
     wrefresh(menuwin);
     keypad(menuwin,TRUE);
 
     borderTB = (yMax/relSize)/2;
     borderLR = (xMax/relSize)/2;
-
-
-    /* int x,y; */
-    /* move(10,10); */
-    /* getyx(stdscr,y,x); */
-    /* printw("\u2B24"); */
 
     getch();
     endwin();
